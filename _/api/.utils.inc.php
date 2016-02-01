d07fd0a980b933c4f8f164e71e2e748573de2679
David Kuo <s50407s@gmail.com>
2016-02-02 00:16:52 +0800

    Utils for api

---
+++ _/api/.utils.inc.php
@@ -0,0 +1,51 @@
+<?php
+class Utils {
+	const HOST = 'https://ncu.one/';
+	
+	const BASE62_HASH = "1QqAazZswCdDvEfYrT4R7F6V5BtUgHbnNMmKjOuW0X9G8iIoJpPLlk23eSxcyh";
+	public static function to_base62($base10) {
+		$base10 = (int)$base10;
+		$result = "";
+		while ($base10 > 0) {
+			$result = self::BASE62_HASH[$base10 % 62] . $result;
+			$base10 = floor($base10 / 62);
+		}
+		return $result;
+	}
+
+	const URL_REGEXP = "/\b(?:(?:https?):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i";
+	public static function check_url($url) {
+		return preg_match(self::URL_REGEXP, $url);
+	}
+
+	const RECAPTCHA_VERIFY_URL = 'https://www.google.com/recaptcha/api/siteverify';
+	public static function recaptcha_verify($secret, $response) {
+		$data = http_build_query(array('secret' => $secret, 'response' => $response));
+		$options = array(
+			'http' => array(
+				'header'  => implode("\r\n", array(
+					"Content-type: application/x-www-form-urlencoded",
+					sprintf("Content-Length: %d", strlen($data))
+				)),
+				'method'  => 'POST',
+				'content' => $data,
+			)
+		);
+		$context = stream_context_create($options);
+		$result = file_get_contents(self::RECAPTCHA_VERIFY_URL, false, $context);
+		if ($result === false) { // cannot connect
+			return array('success' => false);
+		} else {
+			$result = json_decode($result);
+			return array('success' => $result->success);
+		}
+	}
+
+	public static function init_database() {
+		$host = $_ENV['DB_HOST'];
+		$user = $_ENV['DB_USER'];
+		$passwd = $_ENV['DB_PASSWD'];
+		$database = $_ENV['DB_DATABASE'];
+		return new PDO('mysql:host=' . $host . ';dbname=' . $database . ';charset=utf8', $user, $passwd);
+	}
+}
--- _/api/short_it.php
+++ _/api/short_it.php
@@ -1,84 +1,33 @@
 <?php
-const host = 'https://ncu.one/';
-
-// convert a num from base 10 to base 62
-function toBase_62($num) {
-	// $base = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	$base = "1QqAazZswCdDvEfYrT4R7F6V5BtUgHbnNMmKjOuW0X9G8iIoJpPLlk23eSxcyh";
-	$res = "";
-	while ($num) {
-		$res = $base[$num % 62] . $res;
-		$num = floor($num / 62);
-	}
-	return $res;
-}
-
-// url checking
-const URL_REGEXP = "/\b(?:(?:https?):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i";
-function check_url($url) {
-	return preg_match(URL_REGEXP, $url);
-}
-
-// reCAPTCHA
-const RECAPTCHA_VERIFY_URL = 'https://www.google.com/recaptcha/api/siteverify';
-function recaptcha_verify($secret, $response) {
-	$data = http_build_query(array('secret' => $secret, 'response' => $response));
-	$options = array(
-		'http' => array(
-			'header'  => implode("\r\n", array(
-				"Content-type: application/x-www-form-urlencoded",
-				sprintf("Content-Length: %d", strlen($data))
-			)),
-			'method'  => 'POST',
-			'content' => $data,
-		)
-	);
-	$context = stream_context_create($options);
-	$result = file_get_contents(RECAPTCHA_VERIFY_URL, false, $context);
-	if ($result === false) { // cannot connect
-		return array('success' => false);
-	} else {
-		$result = json_decode($result);
-		return array('success' => $result->success);
-	}
-}
-
-// database
-function init_database() {
-	$host = $_ENV['DB_HOST'];
-	$user = $_ENV['DB_USER'];
-	$passwd = $_ENV['DB_PASSWD'];
-	$database = $_ENV['DB_DATABASE'];
-	return new PDO('mysql:host=' . $host . ';dbname=' . $database . ';charset=utf8', $user, $passwd);
-}
+require_once(dirname(__FILE__) . '/.utils.inc.php');
 
 $request = array('url' => $_POST['url'], 'token' => $_POST['captchaToken']);
-if (check_url($request['url']))
-	$result = recaptcha_verify($_ENV['grecaptcha_secret'], $request['token']);
+if (Utils::check_url($request['url']))
+	$result = Utils::recaptcha_verify($_ENV['grecaptcha_secret'], $request['token']);
 else
 	$result = array('success' => false);
 
 if ($result['success'] === true) {
-	$db = init_database();
+	$db = Utils::init_database();
 
 	// check if it has been shortened before
 	$result['shorten'] = (function ($db, $url) {
-		$stmt = $db->prepare('SELECT `id` FROM `ncuone` WHERE `url` = ?');
+		$stmt = $db->prepare('SELECT `id` FROM `ncuone` WHERE `url` = ? LIMIT 1');
 		$stmt->execute(array($url));
 		$id = $stmt->fetch();
 		if ($id === false)
 			return null;
-		return toBase_62($id);
+		return Utils::to_base62($id['id']);
 	})($db, $request['url']);
 
 	// generate a short url if we could not find it in table
 	// then insert it into table
 	if ($result['shorten'] === null) {
 		$guess_id = (function ($db) {
-			$stmt = $db->prepare('SELECT MAX(`id`) FROM `ncuone`'); // count the max id in table
+			$stmt = $db->prepare('SELECT MAX(`id`) as id FROM `ncuone`'); // count the max id in table
 			$stmt->execute();
-			$id = (int)$stmt->fetch();
-			return toBase_62($id + 1);
+			$id = $stmt->fetch()['id'];
+			return Utils::to_base62($id + 1);
 		})($db);
 
 		if (strlen($request['url']) > strlen(host) + strlen($guess_id)) {
@@ -90,7 +39,7 @@ if ($result['success'] === true) {
 					error_log("insert error: " . $stmt->errorInfo()[2]);
 					return null;
 				}
-				return toBase_62($db->lastInsertId());
+				return Utils::to_base62($db->lastInsertId());
 			})($db, $request['url']);
 		}
 	}
@@ -101,6 +50,6 @@ if ($result['success'] === true) {
 
 echo json_encode(array(
 	'status' => $result['success'] ? 'ok' : 'failed',
-	'url' => host . $result['shorten'],
+	'url' => Utils::HOST . $result['shorten'],
 	'code' => $result['shorten']
 ));
