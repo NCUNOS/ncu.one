7adc7043a7e7c14a6420af08f9b213c0f5a5e849
David Kuo <s50407s@gmail.com>
2016-02-02 22:54:52 +0800

    Add http:// first

--- _/api/short_it.php
+++ _/api/short_it.php
@@ -3,13 +3,13 @@ require_once(dirname(__FILE__) . '/.utils.inc.php');
 require_once(dirname(__FILE__) . '/.init.inc.php');
 
 $request = array('url' => $_POST['url'], 'token' => $_POST['captchaToken']);
+if (substr($request['url'], 0, 4) != "http")
+	$request['url'] = 'http://' . $request['url'];
+
 if (Utils::check_url($request['url']))
 	$result = Utils::recaptcha_verify($_ENV['grecaptcha_secret'], $request['token']);
 else
 	$result = array('success' => false);
-
-if (substr($request['url'], 0, 4) != "http")
-	$request['url'] = 'http://' . $request['url'];
 
 if ($result['success'] === true) {
 	$db = Utils::init_database();
